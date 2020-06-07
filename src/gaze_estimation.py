861592c207363a6f0d3cd4259c76ba3bf5db24aa
Kunal Gehlot <dev.ergehlot@gmail.com>
2020-06-07 16:22:11 +0530

    reset everything

--- src/gaze_estimation.py
+++ src/gaze_estimation.py
@@ -1,81 +1,46 @@
-import cv2
-import numpy as np
-import math
-import os
-from openvino.inference_engine import IECore, IENetwork, IEPlugin
+'''
+This is a sample class for a model. You may choose to use it as-is or make any changes to it.
+This has been provided just to give you an idea of how to structure your model class.
+'''
 
-
-class GazeEstimationModel:
-
-    def __init__(self, model_name, threshold, device='CPU', extensions=None, async_mode=True):
-        self.model_name = model_name
-        self.device = device
-        self.extensions = extensions
-        self.model_weights = self.model_name.split('.')[0]+'.bin'
-        self.plugin = None
-        self.network = None
-        self.exec_network = None
-        self.input_name = None
-        self.input_shape = None
-        self.output_name = None
-        self.output_shape = None
-        self.threshold = threshold
-        self.initial_w = None
-        self.initial_h = None
-        self.async_mode = async_mode
+class Model_X:
+    '''
+    Class for the Face Detection Model.
+    '''
+    def __init__(self, model_name, device='CPU', extensions=None):
+        '''
+        TODO: Use this to set your instance variables.
+        '''
+        raise NotImplementedError
 
     def load_model(self):
-        self.plugin = IEPlugin(device=self.device)
-        if self.extensions and "CPU" in self.device:
-            self.plugin.add_cpu_extension(self.extensions)
-        self.network = IENetwork(
-            model=self.model_name, weights=self.model_weights)
-        self.check_plugin(self.plugin)
-        self.exec_network = self.plugin.load(self.network)
-        self.input_pose_angles = self.network.inputs['head_pose_angles']
-        self.output_name = next(iter(self.network.outputs))
-        self.output_shape = self.network.outputs[self.output_name].shape
+        '''
+        TODO: You will need to complete this method.
+        This method is for loading the model to the device specified by the user.
+        If your model requires any Plugins, this is where you can load them.
+        '''
+        raise NotImplementedError
 
-    def check_plugin(self, plugin):
-        unsupported_layers = [l for l in self.network.layers.keys(
-        ) if l not in self.plugin.get_supported_layers(self.network)]
-        if len(unsupported_layers) != 0:
-            print("Unsupported layers found: {}".format(unsupported_layers))
-            exit(1)
+    def predict(self, image):
+        '''
+        TODO: You will need to complete this method.
+        This method is meant for running predictions on the input image.
+        '''
+        raise NotImplementedError
 
-    def predict(self, lEyeImg, rEyeImg, pose):
-        count = 0
-        coords = None
-        self.initial_w = lEyeImg.shape[1]
-        self.initial_h = lEyeImg.shape[0]
-        lEyeImg, rEyeImg = self.process_in(
-            lEyeImg, rEyeImg)
-        if self.async_mode:
-            self.exec_network.requests[0].async_infer(inputs={"head_pose_angles": pose, "left_eye_image": lEyeImg,
-                                                              "right_eye_image": rEyeImg})
-        else:
-            self.exec_network.requests[0].infer(inputs={"head_pose_angles": pose, "left_eye_image": lEyeImg,
-                                                        "right_eye_image": rEyeImg})
-        if self.exec_network.requests[0].wait(-1) == 0:
-            outputs = self.exec_network.requests[0].outputs[self.output_name]
-            out = self.process_out(outputs)
-            return out
+    def check_model(self):
+        raise NotImplementedError
 
-    def process_in(self, lEyeImg, rEyeImg):
-        lEyeImg = cv2.resize(lEyeImg, (60, 60))
-        lEyeImg = lEyeImg.transpose((2, 0, 1))
-        lEyeImg = lEyeImg.reshape((1, 3, 60, 60))
-        rEyeImg = cv2.resize(rEyeImg, (60, 60))
-        rEyeImg = rEyeImg.transpose((2, 0, 1))
-        rEyeImg = rEyeImg.reshape((1, 3, 60, 60))
-        return lEyeImg, rEyeImg
+    def preprocess_input(self, image):
+    '''
+    Before feeding the data into the model for inference,
+    you might have to preprocess it. This function is where you can do that.
+    '''
+        raise NotImplementedError
 
-    def process_out(self, outputs):
-        gaze_vector = outputs[0]
-        roll = gaze_vector[2]
-        gaze_vector = gaze_vector / np.linalg.norm(gaze_vector)
-        cs = math.cos(roll * math.pi / 180.0)
-        sn = math.sin(roll * math.pi / 180.0)
-        tmpX = gaze_vector[0] * cs + gaze_vector[1] * sn
-        tmpY = -gaze_vector[0] * sn + gaze_vector[1] * cs
-        return (tmpX, tmpY), (gaze_vector)
+    def preprocess_output(self, outputs):
+    '''
+    Before feeding the output of this model to the next model,
+    you might have to preprocess the output. This function is where you can do that.
+    '''
+        raise NotImplementedError
--- src/head_pose_estimation.py
+++ src/head_pose_estimation.py
@@ -1,77 +1,46 @@
-import cv2
-import numpy as np
-import math
-import os
-from openvino.inference_engine import IECore, IENetwork, IEPlugin
+'''
+This is a sample class for a model. You may choose to use it as-is or make any changes to it.
+This has been provided just to give you an idea of how to structure your model class.
+'''
 
-
-class HeadPoseEstimationModel:
-    def __init__(self, model_name, threshold, device='CPU', extensions=None, async_mode=True):
-        self.model_name = model_name
-        self.device = device
-        self.extensions = extensions
-        self.model_weights = self.model_name.split('.')[0]+'.bin'
-        self.plugin = None
-        self.network = None
-        self.exec_network = None
-        self.input_name = None
-        self.input_shape = None
-        self.output_name = None
-        self.output_shape = None
-        self.threshold = threshold
-        self.initial_w = None
-        self.initial_h = None
-        self.async_mode = async_mode
+class Model_X:
+    '''
+    Class for the Face Detection Model.
+    '''
+    def __init__(self, model_name, device='CPU', extensions=None):
+        '''
+        TODO: Use this to set your instance variables.
+        '''
+        raise NotImplementedError
 
     def load_model(self):
-        self.plugin = IEPlugin(device=self.device)
-        if self.extensions and "CPU" in self.device:
-            self.plugin.add_cpu_extension(self.extensions)
-        self.network = IENetwork(
-            model=self.model_name, weights=self.model_weights)
-        self.check_plugin(self.plugin)
-        self.exec_network = self.plugin.load(self.network)
-        self.input_name = next(iter(self.network.inputs))
-        self.output_name = next(iter(self.network.outputs))
-        self.output_shape = self.network.outputs[self.output_name].shape
+        '''
+        TODO: You will need to complete this method.
+        This method is for loading the model to the device specified by the user.
+        If your model requires any Plugins, this is where you can load them.
+        '''
+        raise NotImplementedError
 
     def predict(self, image):
-        count = 0
-        coords = None
-        self.initial_w = image.shape[1]
-        self.initial_h = image.shape[0]
-        frame = self.preprocess_input(image)
-        if self.async_mode:
-            self.exec_network.requests[0].async_infer(
-                inputs={self.input_name: frame})
-        else:
-            self.exec_network.requests[0].infer(
-                inputs={self.input_name: frame})
-        if self.exec_network.requests[0].wait(-1) == 0:
-            outputs = self.exec_network.requests[0].outputs
-            flag, pose = self.preprocess_output(image, outputs)
-            return flag, pose
+        '''
+        TODO: You will need to complete this method.
+        This method is meant for running predictions on the input image.
+        '''
+        raise NotImplementedError
 
-    def check_plugin(self, plugin):
-        unsupported_layers = [l for l in self.network.layers.keys(
-        ) if l not in self.plugin.get_supported_layers(self.network)]
-        if len(unsupported_layers) != 0:
-            print("Unsupported layers found: {}".format(unsupported_layers))
-            exit(1)
+    def check_model(self):
+        raise NotImplementedError
 
     def preprocess_input(self, image):
-        (n, c, h, w) = self.network.inputs[self.input_name].shape
-        frame = cv2.resize(image, (w, h))
-        frame = frame.transpose((2, 0, 1))
-        frame = frame.reshape((n, c, h, w))
-        return frame
+    '''
+    Before feeding the data into the model for inference,
+    you might have to preprocess it. This function is where you can do that.
+    '''
+        raise NotImplementedError
 
-    def preprocess_output(self, image, outputs):
-        xAngle = outputs["angle_p_fc"][0]
-        yAngle = outputs["angle_y_fc"][0]
-        zAngle = outputs["angle_r_fc"][0]
-        if ((yAngle > -22.5) & (yAngle < 22.5) & (xAngle > -22.5) &
-                (xAngle < 22.5)):
-            return True, [[yAngle, xAngle, zAngle]]
-        else:
-            return False, [[0, 0, 0]]
+    def preprocess_output(self, outputs):
+    '''
+    Before feeding the output of this model to the next model,
+    you might have to preprocess the output. This function is where you can do that.
+    '''
+        raise NotImplementedError
